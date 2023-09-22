2a1f16f4d4d9277a458b21baa8fe444bebd76a75
bintangbin <142990997+bintangbin@users.noreply.github.com>
2023-09-22 11:58:29 +0700

    Add files via upload
    saya mengupload file

---
+++ bintang3.js
@@ -0,0 +1,8 @@
+function popupku() {
+  var body = document.querySelector("body");      
+  var pWm = document.createElement("a");
+  pWm.setAttribute("href", "https://instagram.aribintang_");
+  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
+  pWm.innerHTML = "dari bintang";
+  body.appendChild(pWm);
+}
